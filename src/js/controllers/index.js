e8da5f3a6cc25129ef5157e29e08282aeecf8b44
Kosta Korenkov <7r0ggy@gmail.com>
2015-11-22 14:11:35 +0300

    Allow to change wallet asset

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -356,7 +356,6 @@ angular.module('copayApp.controllers').controller('indexController',
         self.walletStatus = walletStatus.wallet.status;
         self.walletScanStatus = walletStatus.wallet.scanStatus;
         self.copayers = walletStatus.wallet.copayers;
-        self.walletAsset = assetsService.walletAsset();
         self.preferences = walletStatus.preferences;
         self.setBalance(walletStatus.balance);
         self.otherWallets = lodash.filter(profileService.getWallets(self.network), function(w) {
@@ -364,8 +363,10 @@ angular.module('copayApp.controllers').controller('indexController',
         });
 
 
-        $rootScope.$on('ColoredCoins/AssetsUpdated', function(event, assets) {
-          assets = lodash.filter(assets, function(asset) {
+        function updateAssetBalance(event) {
+          self.walletAsset = assetsService.walletAsset();
+
+          var assets = lodash.filter(coloredCoins.assets, function(asset) {
             return asset.assetId == self.walletAsset;
           });
 
@@ -375,7 +376,10 @@ angular.module('copayApp.controllers').controller('indexController',
           }, 0);
 
           self.totalAssetBalanceStr = coloredCoins.formatAssetAmount(coloredBalance, assets[0]);
-        });
+        }
+
+        $rootScope.$on('ColoredCoins/AssetsUpdated', updateAssetBalance);
+        $rootScope.$on('Local/WalletAssetUpdated', updateAssetBalance);
 
         // Notify external addons or plugins
         $rootScope.$emit('Local/BalanceUpdated', walletStatus.balance);
